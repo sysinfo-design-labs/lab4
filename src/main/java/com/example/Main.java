902875165c24277580a418fb98ba91727e71863c
roman <j-kio@mail.ru>
2025-09-23 19:52:00 +0500

    initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+
+    public static String helloWorld() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+    }
+}
